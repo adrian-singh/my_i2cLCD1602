fb7c20dd3a9c19a78de10776509b2d20e260634a
adrian-singh <singh_adrian_shopping@hotmail.co.uk>
2022-08-27 22:26:18 +0100

    Update LCD1602_I2C.ts

--- LCD1602_I2C.ts
+++ LCD1602_I2C.ts
@@ -130,7 +130,7 @@ namespace MY_I2C_LCD1602 {
     //% imageLiteralColumns=5
     //% imageLiteralRows=8
     //% shim=images::createImage
-    export function makeCustom(n: number, i: string): void {
+    export function makeCustom(i: string, n: number): void {
         //...to do...
         console.logValue("n:", n) 
         console.logValue("i:", i) 
